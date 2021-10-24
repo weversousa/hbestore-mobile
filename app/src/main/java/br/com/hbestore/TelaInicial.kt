2a1bc7a2c71b0d050ba2c1cb67027ff79cba9eaa
Lucas <lucas.tavares@smartbrain.com.br>
2021-10-24 13:31:43 -0300

    Recyclew View com Card View dinamico

--- app/src/main/java/br/com/hbestore/TelaInicial.kt
+++ app/src/main/java/br/com/hbestore/TelaInicial.kt
@@ -2,9 +2,20 @@ package br.com.hbestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.navigation.NavigationBarItemView
 import kotlinx.android.synthetic.main.activity_tela_inicial.*
 
+
 class TelaInicial : AppCompatActivity() {
+    private var layoutManager: RecyclerView.LayoutManager? = null
+    private var adapter: RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tela_inicial)
@@ -16,6 +27,16 @@ class TelaInicial : AppCompatActivity() {
         val parametro_usuario = parametros_tela_login?.getString("parametro_usuario")
         val parametro_senha = parametros_tela_login?.getString("parametro_senha")
 
-        mostrar.text = "$parametro_usuario $parametro_senha"
+        // mostrar.text = "$parametro_usuario $parametro_senha"
+
+
+        layoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = layoutManager
+
+        adapter = RecyclerViewAdapter()
+        recyclerView.adapter = adapter
+
     }
+
+
 }
