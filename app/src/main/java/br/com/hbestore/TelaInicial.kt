f429586d36f740ad1756e553dad198f0a0dc0c70
Weverton Teixeira <weverton.tsousa@hotmail.com>
2021-10-27 18:11:56 -0300

    atualizado

--- app/src/main/java/br/com/hbestore/TelaInicial.kt
+++ app/src/main/java/br/com/hbestore/TelaInicial.kt
@@ -2,19 +2,47 @@ package br.com.hbestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.navigation.NavigationBarItemView
 import kotlinx.android.synthetic.main.activity_tela_inicial.*
+import kotlinx.android.synthetic.main.toolbar.*
 
 
 class TelaInicial : AppCompatActivity() {
     private var layoutManager: RecyclerView.LayoutManager? = null
     private var adapter: RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>? = null
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_tela_inicial, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    // Função que captura cliques na barra de Menu
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        // Armazenar o ID do item clicado na barra de Menu
+        val id = item.itemId
+
+        // Descobrir qual item foi clicado através do seu ID e aplicar a ação que deseja para ele
+        if (id == R.id.menu_item_buscar) {
+            Toast.makeText(this, "Botão buscar", Toast.LENGTH_SHORT).show()
+        } else if (id == R.id.menu_item_atualizar) {
+            Toast.makeText(this, "Botão atualizar", Toast.LENGTH_SHORT).show()
+        } else if (id == R.id.menu_item_configuracao) {
+            Toast.makeText(this, "Botão Config", Toast.LENGTH_SHORT).show()
+        } else if (id === android.R.id.home) {
+            // Se for clicado na seta de voltar (esse ID é fornecido pela aplicação não somos nós que criamos)
+
+            // método que destrói a Activity atual, se ele vai destruir a atual a tela que será exibida é a anterior
+            finish()
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +56,16 @@ class TelaInicial : AppCompatActivity() {
         val parametro_senha = parametros_tela_login?.getString("parametro_senha")
 
         // mostrar.text = "$parametro_usuario $parametro_senha"
+
+
+        // Importando a nossa Toolbar para ser exibida na tela
+        setSupportActionBar(toolbar)
+
+        // Título que será exibido na tela de Menu
+        supportActionBar?.title = "Produtos"
+
+        // Inserir seta de voltar na barra de Menu
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
 
         layoutManager = LinearLayoutManager(this)
